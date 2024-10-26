0b37963539c7c798c2b3ed1e72e8ddf7840f1fe4
chandru1227 <chandruarchunan04@gmail.com>
2024-10-26 22:41:52 +0530

    404 page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,27 @@
 import Sidebar from "./component/sidebar/Sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LandHome } from "./component/landingpage/LandHome";
-import Dashboardnav from "./component/pages/Dashboardnav";
-import Overview from './component/pages/Overview'
-import { Outlet } from "react-router-dom";
+import Overview from './component/pages/Overview';
 import SignInSignUp from "./component/signin";
-// import Leetcode from "./component/pages/Leetcode";
-// import YouTubeDashboard from "./component/pages/Youtube";
 import YouTubeForm from "./component/pages/Youtube";
+import NotFound from "./component/notFound/NotFound";
 
 export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandHome />} />
-          <Route path="/signin" element={<SignInSignUp />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandHome />} />
+        <Route path="/signin" element={<SignInSignUp />} />
 
-          <Route exact path="/das" element={<Sidebar />}>
-            <Route index element={<Overview />} />
-            {/* <Route path="friends" element={<Friends />} /> */}
-            <Route path="youtube" element={<YouTubeForm />} />
-            
-          </Route>
-          {/* <Route path="/leetcode" element={<Leetcode />} /> */}
+        <Route path="/das" element={<Sidebar />}>
+          <Route index element={<Overview />} />
+          <Route path="youtube" element={<YouTubeForm />} />
+          {/* Add more routes here as needed */}
+        </Route>
 
-          <Route path="/sddd" element={<LandHome />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+        {/* Catch-all route for Not Found */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
